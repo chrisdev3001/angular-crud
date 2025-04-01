675f09e252d6852bb2a9fe1b6a94f8a1f9e30a3e
Chris Villarroel <chrisdev3001@gmail.com>
2025-04-01 15:02:54 -0300

    feat: add render characters on list page + dummy api data

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+import { Character, CharacterList } from '../../types';
+import { LocalStorageService } from '../../services/local-storage.service';
+import * as dummyResponseAPI from '../../../dummy/data.json';
+
+const API_DATA_KEY_STATE = 'api-data'
 
 @Component({
   selector: 'app-list',
-  imports: [],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+  data = [] as unknown as CharacterList
 
+  constructor(private localStorageService: LocalStorageService) {}
+
+  setCharacters(freshData: CharacterList) {
+    this.data = freshData
+    this.localStorageService.setItem(API_DATA_KEY_STATE, freshData)
+  }
+
+  ngOnInit() {
+    if (this.localStorageService.getItem(API_DATA_KEY_STATE)){
+      this.data = this.localStorageService.getItem(API_DATA_KEY_STATE) as unknown as CharacterList
+    }else{
+      // call the api
+      // ... TODO: call the api
+      this.setCharacters(dummyResponseAPI as unknown as CharacterList)
+    }    
+
+    console.log(this.data, 'this.data')
+  }
 }
