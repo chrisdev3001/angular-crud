829e7a18c31efdcc87abf9f6138d333ab775512e
Chris Villarroel <chrisdev3001@gmail.com>
2025-04-01 14:26:50 -0300

    feat: add pages + navbar component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AddEditComponent } from './pages/add-edit/add-edit.component';
+import { DetailComponent } from './pages/detail/detail.component';
+import { ListComponent } from './pages/list/list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'list', component: ListComponent },
+  { path: 'edit', component: AddEditComponent },
+  { path: 'detail', component: DetailComponent },
+];
